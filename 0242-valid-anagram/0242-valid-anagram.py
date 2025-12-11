f9a7b917c42394418fc25d74dc9db4652d4da4d2
muramneelima-2007 <muramneelima8@gmail.com>
2025-12-11 12:24:49 +0530

    Time: 7 ms (89.97%), Space: 18.1 MB (23.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # p=Counter(s)
-        # c=Counter(t)
-        # if(list(p.keys()).sort()!=list(c.keys()).sort()):
-        #     return False
-        # for key,value in c.items():
-        #     if(c[key]==p[key]):
-        #         continue
-        #     else:
-        #         return False
-        # return True 
-        p=[]
-        c=[]
-        for i in s:
-            p=p+[i]
-        for i in t:
-            c=c+[i]
-        p.sort()
-        c.sort()
-        if(p==c):
-            return True
-        return False
+        p=Counter(s)
+        c=Counter(t)
+        if(sorted(p.keys()) != sorted(c.keys())):
+            return False
+        for key,value in c.items():
+            if(c[key]==p[key]):
+                continue
+            else:
+                return False
+        return True 
+
+        # p=[]
+        # c=[]
+        # for i in s:
+        #     p=p+[i]
+        # for i in t:
+        #     c=c+[i]
+        # p.sort()
+        # c.sort()
+        # if(p==c):
+        #     return True
+        # return False
 
 
     
