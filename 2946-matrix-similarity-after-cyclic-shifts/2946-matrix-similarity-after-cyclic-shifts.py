f3d24a67ee778d4bcf441367b3f914468e71c79c
muramneelima-2007 <237682077+muramneelima-2007@users.noreply.github.com>
2026-06-01 10:41:37 +0530

    Time: 12 ms (7.32%), Space: 19.4 MB (12.04%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -5,11 +5,10 @@ class Solution:
         while(k!=0):
             matrix=[]
             for i in range(m):
+                li=mat[i]
                 if(i%2==0):
-                    li=mat[i]
                     li=li[1:]+[li[0]]
                 else:
-                    li=mat[i]
                     li=[li[-1]]+li[:-1]
                 matrix.append(li)
             mat=matrix
