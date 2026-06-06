3d6899e1fc3907c05f849cec544b3c63a2217017
muramneelima-2007 <237682077+muramneelima-2007@users.noreply.github.com>
2026-06-06 20:48:11 +0530

    Time: 3 ms (74.11%), Space: 19.6 MB (23.2%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -12,36 +12,36 @@ class Solution:
         #     curr=abs((s[j]-nums[j])-(las-s[j]))
         #     res.append(curr)
         # return res
-        # n=len(nums)
-        # la=[]
-        # ra=[]
-        # sum1=0
-        # summ=sum(nums)
-        # la=[sum1]
-        # for i in range(n):
-        #     sum1+=nums[i]
-        #     la.append(sum1)
-        #     ra.append(summ-sum1)
-        # res=[]
-        # for i in range(n):
-        #     res.append(abs(la[i]-ra[i]))
-        # return res
+        n=len(nums)
+        la=[]
+        ra=[]
+        sum1=0
+        summ=sum(nums)
+        la=[sum1]
+        for i in range(n):
+            sum1+=nums[i]
+            la.append(sum1)
+            ra.append(summ-sum1)
+        res=[]
+        for i in range(n):
+            res.append(abs(la[i]-ra[i]))
+        return res
         
-        n = len(nums)
-        left = [0] * n
-        right = [0] * n
-        fini = [0] * n
+        # n = len(nums)
+        # left = [0] * n
+        # right = [0] * n
+        # fini = [0] * n
 
-        sum1 = 0
-        sum2 = 0
+        # sum1 = 0
+        # sum2 = 0
 
-        for i in range(n):
-            sum1 += nums[i]
-            sum2 += nums[n - i - 1]
-            left[i] = sum1
-            right[n - i - 1] = sum2
+        # for i in range(n):
+        #     sum1 += nums[i]
+        #     sum2 += nums[n - i - 1]
+        #     left[i] = sum1
+        #     right[n - i - 1] = sum2
 
-        for i in range(n):
-            fini[i] = abs(left[i] - right[i])
+        # for i in range(n):
+        #     fini[i] = abs(left[i] - right[i])
 
-        return fini
+        # return fini
